606936d2c93b313569fa7a7ac0e8d8214d8c187f
itacode <daiasobi@hotmail.com>
2018-08-27 21:53:46 +0900

    Enabled Vuex built-in logger plugin

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,14 +1,17 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createLogger from 'vuex/dist/logger';
 
 Vue.use(Vuex);
 
 const debug = process.env.NODE_ENV !== 'production';
+const plugins = debug ? [createLogger({})] : [];
 const store = new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
   strict: debug,
+  plugins,
 });
 
 export default store;
