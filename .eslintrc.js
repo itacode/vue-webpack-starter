605b222d3e537af6cd767725f0b3f1584036b9af
itacode <>
2019-01-28 08:01:06 +0900

    update

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+    mocha: true,
+    jquery: true,
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:vue/essential',
+  ],
+  rules: { // "off" or 0; "warn" or 1; "error" or 2
+    'no-console': process.env.NODE_ENV !== 'production' ? 'off' : 'error',
+    indent: [
+      'error',
+      2,
+    ],
+    'linebreak-style': [
+      'error',
+      'unix',
+    ],
+    quotes: [
+      'error',
+      'single',
+    ],
+    semi: [
+      'error',
+      'always',
+    ],
+  },
+};
