7cf19a46119e3cdf41a11d8696e02ae26e090564
itacode <>
2019-10-27 09:35:54 +0900

    Add unit debug

--- src/app/views/__tests__/foo.spec.js
+++ src/app/views/__tests__/foo.spec.js
@@ -7,6 +7,7 @@ import '../../plugins';
 describe('Foo.fetchedData', () => {
   test('sanity test', () => {
     const wrapper = mount(foo);
+    debugger;
     expect(wrapper.vm.fetchedData).toBe(null);
   });
 });
