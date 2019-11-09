9b3bc35a6f194e0725fa9bc4c5be186ab3912596
itacode <>
2019-11-09 17:01:47 +0900

    Adopt gulp browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,59 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const browserSync = require('browser-sync');
 
-var sassSource = './src/css/**/*.scss',
-  cssDest = './src/css';
+const paths = {
+  sassSource: './src/css/**/*.scss',
+  cssDest: './src/css',
+  htmlSource: './src/index.html',
+};
+
+const server = browserSync.create();
 
 function style() {
-  var stream = gulp
-    .src(sassSource, { since: gulp.lastRun(style) })
+  const stream = gulp
+    .src(paths.sassSource, { since: gulp.lastRun(style) })
     .pipe(
       sass({
         outputStyle: 'expanded',
       }).on('error', sass.logError)
     )
     .pipe(postcss([autoprefixer()]))
-    .pipe(gulp.dest(cssDest));
+    .pipe(gulp.dest(paths.cssDest))
+    .pipe(server.stream());
   return stream;
 }
 
+function reload(cb) {
+  server.reload();
+  cb();
+}
+
+function serve(cb) {
+  server.init({
+    server: {
+      baseDir: './src',
+    },
+  });
+  cb();
+}
+
 function watch(cb) {
-  gulp.watch(sassSource, style).on('change', function(path) {
+  gulp.watch(paths.sassSource, style).on('change', function(path) {
     console.log('Changed: ' + path);
   });
   cb();
 }
 
+function watchForServer(cb) {
+  gulp.watch(paths.htmlSource, reload);
+  cb();
+}
+
+const dev = gulp.series(watch, serve, watchForServer);
+
 module.exports.style = style;
 module.exports.watch = watch;
+module.exports.default = dev;
