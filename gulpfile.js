15702d651726f66d720c7a0cfc15e851353e55be
itacode <>
2019-11-09 15:06:38 +0900

    Adopt lastRun

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
 var gulp = require('gulp');
-var cache = require('gulp-cached');
-var progeny = require('gulp-progeny');
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
@@ -9,20 +7,20 @@ var sassSource = './src/css/**/*.scss',
   cssDest = './src/css';
 
 function style() {
-  var stream = gulp.src(sassSource)
-    .pipe(cache('style'))
-    .pipe(progeny())
-    .pipe(sass({
-        outputStyle: 'expanded'
-      })
-      .on('error', sass.logError))
+  var stream = gulp
+    .src(sassSource, { since: gulp.lastRun(style) })
+    .pipe(
+      sass({
+        outputStyle: 'expanded',
+      }).on('error', sass.logError)
+    )
     .pipe(postcss([autoprefixer()]))
     .pipe(gulp.dest(cssDest));
   return stream;
 }
 
 function watch(cb) {
-  gulp.watch(sassSource, style).on('change', function (path) {
+  gulp.watch(sassSource, style).on('change', function(path) {
     console.log('Changed: ' + path);
   });
   cb();
