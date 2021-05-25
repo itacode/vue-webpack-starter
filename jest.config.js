dc76510d491055be8faacc5336739a4bba201343
itacode <>
2021-05-25 21:07:08 +0900

    Updage packages

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 // For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
+// https://jestjs.io/docs/configuration
 
 module.exports = {
   // A map from regular expressions to paths to transformers
@@ -9,4 +9,7 @@ module.exports = {
     // process js with `babel-jest`
     '^.+\\.js$': 'babel-jest',
   },
+  // From jest 27 it defaults to node.
+  // Jsdom is for a browser environment.
+  testEnvironment: 'jsdom',
 };
