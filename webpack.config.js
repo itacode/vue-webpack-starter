eadfc082d7dbad81edeacd4e425d6b0c6fb7ae62
itacode <>
2022-03-26 17:56:29 +0900

    Change asset conf for images

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ module.exports = (env, argv) => {
         },
         {
           test: /\.(png|jpg|jpeg|gif|svg)$/i,
-          type: 'asset/resource',
+          type: 'asset',
         },
       ],
     },
