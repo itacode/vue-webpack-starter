ad4b7ec7a7d93d7668a797e2cd30745039f80e9f
itacode <9909272+itacode@users.noreply.github.com>
2026-07-18 15:59:38 +0900

    adjust eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,18 @@
 import js from '@eslint/js';
-import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import pluginVue from 'eslint-plugin-vue';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default defineConfig([
+  globalIgnores(['dist/']),
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts,vue}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      pluginVue.configs['flat/recommended'],
+      eslintConfigPrettier,
+    ],
   },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  tseslint.configs.recommended,
-  pluginVue.configs['flat/essential'],
-  {
-    files: ['**/*.vue'],
-    languageOptions: { parserOptions: { parser: tseslint.parser } },
-  },
-  eslintConfigPrettier,
 ]);
